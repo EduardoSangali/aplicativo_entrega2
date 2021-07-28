61c5f76f817530d084c08a4f4fccdbc915da8ced
Cristiane Danna <cristiane.danna@gmail.com>
2021-07-27 21:23:00 -0300

    first instrumented test

---
+++ app/src/androidTest/java/com/example/appteste1/AppointmentAdapterInstrumentedTest.kt
@@ -0,0 +1,76 @@
+package com.example.appteste1
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.appteste1.model.bean.Agendamento
+import com.example.appteste1.ui.appointment.AppointmentListAdapter
+import com.example.appteste1.ui.appointment.AppointmentViewModel
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+
+
+@RunWith(AndroidJUnit4::class)
+class AppointmentAdapterInstrumentedTest {
+    lateinit var mAdapter : AppointmentListAdapter
+
+    lateinit var viewModel: AppointmentViewModel
+    lateinit var viewGroup: ViewGroup
+
+    private var agendamento1: Agendamento = Agendamento("","","","","","")
+    private var agendamento2: Agendamento = Agendamento("","","","","","")
+
+    @Before
+    fun setUp() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        viewModel = AppointmentViewModel()
+        viewGroup = ListView(appContext)
+
+        val data: ArrayList<Agendamento> = ArrayList<Agendamento>()
+        agendamento1 = Agendamento("1254685","28/07/2021 10:30", "Fisioterapia", "teste","Dr. João Miguel Saran", "12546325")
+        agendamento2 = Agendamento("9853124","25/08/2021 11:30", "Acupuntura", "teste","Dr. João Miguel Saran", "12546953")
+        data.add(agendamento1)
+        data.add(agendamento2)
+        mAdapter = AppointmentListAdapter(appContext, R.layout.list_item, data, false, false, false, viewModel)
+    }
+
+    @Test
+    fun testGetItem() {
+        assertEquals(
+            "Fisioterapia was expected.", agendamento1.procedim,
+            (mAdapter?.getItem(0) as Agendamento).procedim
+        )
+    }
+
+    @Test
+    fun testGetItemId() {
+        assertEquals("Wrong ID.", 0, mAdapter?.getItemId(0))
+    }
+
+    @Test
+    fun testGetView() {
+        val view: View = mAdapter.getView(0, null, viewGroup)
+
+        val dataProced: TextView = view.findViewById(R.id.listaDataHora)
+        val nomeProced: TextView = view.findViewById(R.id.listaEspecialidade2)
+
+        //On this part you will have to test it with your own views/data
+        assertNotNull("View is null. ", view)
+        assertNotNull("Procedure Date TextView is null. ", dataProced)
+        assertNotNull("Procedure Name TextView is null. ", nomeProced)
+        assertEquals("Procedure Names doesn't match.",
+            agendamento1.procedim, nomeProced.text)
+        assertEquals("Procedure Date doesn't match.",
+            agendamento1.dataHora, dataProced.text
+        )
+    }
+}
